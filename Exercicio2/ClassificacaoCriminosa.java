d3034a0c1b986d003d177a5eb6998f123b021bb5
Andreoly Jesus <andreolyjesus@hotmail.com>
2023-04-03 11:56:59 -0300

    Collection List

---
+++ Exercicio2/ClassificacaoCriminosa.java
@@ -0,0 +1,62 @@
+package Exercicio2;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClassificacaoCriminosa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<String> respostas = new ArrayList<>();
+        System.out.print("Telefonou para a vítima? (sim/não) ");
+        String resposta = sc.nextLine().toLowerCase();
+        respostas.add(resposta);
+
+        System.out.print("Esteve no local do crime? (sim/não) ");
+        resposta = sc.nextLine().toLowerCase();
+        respostas.add(resposta);
+
+        System.out.print("Mora perto da vítima? (sim/não) ");
+        resposta = sc.nextLine().toLowerCase();
+        respostas.add(resposta);
+
+        System.out.print("Devia para a vítima? (sim/não) ");
+        resposta = sc.nextLine().toLowerCase();
+        respostas.add(resposta);
+
+        System.out.print("Já trabalhou com a vítima? (sim/não) ");
+        resposta = sc.nextLine().toLowerCase();
+        respostas.add(resposta);
+
+        int respostasPositivas = contarRespostasPositivas(respostas);
+        switch (respostasPositivas) {
+            case 2:
+                System.out.println("Classificação: Suspeita");
+                break;
+            case 3:
+            case 4:
+                System.out.println("Classificação: Cúmplice");
+                break;
+            case 5:
+                System.out.println("Classificação: Assassina");
+                break;
+            default:
+                System.out.println("Classificação: Inocente");
+                break;
+        }
+    }
+
+    public static int contarRespostasPositivas(List<String> respostas) {
+        int contador = 0;
+        for (String resposta : respostas) {
+            if (resposta.equals("sim")) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
+
+
